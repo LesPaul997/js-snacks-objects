e4711c0fc9856f669380a2d649a9943a29507163
LesPaul997 <mariogiordano941@gmail.com>
2024-05-22 14:26:18 +0200

    add : ciclo for and if

--- snack4/main.js
+++ snack4/main.js
@@ -7,38 +7,56 @@ const identityPeople = [
         nome: 'Eugenio',
         cognome: 'Scognamiglio',
         età: '20',
-        info: '',
     },
     {
         nome: 'Mario',
         cognome: 'Giordano',
         età: '27',
-        info: '',
     },
     {
         nome: 'Gaetano',
         cognome: 'Pucci',
         età: '15',
-        info: '',
     },
     {
         nome: 'Marco',
         cognome: 'Soldo',
         età: '17',
-        info: '',
     },
     {
         nome: 'Giuseppe',
         cognome: 'Ancelotti',
         età: '55',
-        info: '',
     },
     {
         nome: 'Francesco',
         cognome: 'Guida',
         età: '37',
-        info: '',
     },
 
 ]
 
+//console.log(identityPeople);
+
+
+//Creo un nuovo array che conterrà le nuove info
+//const importantInfo = [];
+
+//Inizio del ciclo 
+for (let i = 0; i < identityPeople.length; i++) {
+    const licensedPerson = identityPeople[i];
+
+//Istruzione condizionale che inserirà all'interno del nuovo array le informazioni aggiornate
+    if (licensedPerson.età >= '18') {
+        licensedPerson.info = `${licensedPerson.nome} ${licensedPerson.cognome} Patentato`;
+    } else {
+        licensedPerson.info = `${licensedPerson.nome} ${licensedPerson.cognome} Non patentato`;
+    }
+}
+
+/* console.log(patented);
+console.log(notPatented); */
+console.log(identityPeople);
+
+//PUSHO IL LAVORO, MA VORREI MODIFICARLO
+//L'INTENZIONE E' QUELLA DI CREARE UN NUOVO ARRAY LASCIANDO INVARIATO QUELLO ORIGINALE, INVECE IN QUESTO CASO ANCHE L'ORIGINALE SUBISCE MODIFICA.
